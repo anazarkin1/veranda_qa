647b498eba7265b79a06fc094678c3b616dc8689
NoahNU <noahnu@gmail.com>
2016-11-28 14:19:16 -0500

    Improved top bar. Added states. #9

--- app/src/js/components/TopNavigationBar.js
+++ app/src/js/components/TopNavigationBar.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 
+const NavigationButton = props => (
+	<div
+		className='navigation-btn'
+		onClick={props.onClick}
+	>{props.label}</div>
+);
+
 export default class TopNavigationBar extends Component {
 	constructor() {
 		super();
 
-		this.state = {};
+		this.state = {
+			registered: true
+		};
+
+		this.logout = this.logout.bind(this);
+	}
+
+	logout() {
+		this.setState({
+			registered: false
+		});
 	}
 
 	render() {
 		return (
 			<nav className='top-navigation-bar'>
 				<span>Veranda: Top Navigation Bar</span>
+				{ this.state.registered && (
+					<NavigationButton
+						label="Logout"
+						onClick={this.logout}
+					/>
+				) }
 			</nav>
 		);
 	}
