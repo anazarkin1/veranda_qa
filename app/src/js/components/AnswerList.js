d98bdb5b2767dab83bddc7669ea657c00c0185d2
Alex Nazarkin <anazarkin1@gmail.com>
2016-12-02 17:32:08 -0500

    add is_anon checkbox to AnswerNew and refactor Answer components into AnswersThread

--- app/src/js/components/AnswerList.js
+++ app/src/js/components/AnswerList.js
@@ -1,35 +1,26 @@
-import React, { Component } from 'react';
-
-import Answer from './Answer';
+import React, {Component} from "react";
+import Answer from "./Answer";
 
 export default class AnswerList extends Component {
-	constructor() {
-		super();
+    constructor() {
+        super();
+    }
 
-		this.state = {
-			answers: []
-		};
-	}
 
-	componentDidMount() {
-		// TODO: Replace Dummy data.
-		this.setState({ answers: [
-			{ id: 1, created_at: ((+new Date())/1000) - 1000 },
-			{ id: 2, created_at: ((+new Date())/1000) - 5000 },
-			{ id: 3, created_at: ((+new Date())/1000) - 10000 }
-		] });
-	}
+    componentDidMount() {
+        // TODO: Replace Dummy data.
+    }
 
-	render() {
-		return (
-			<div className='answer-list'>
-				{this.state.answers.map(answer => (
-					<Answer
-						key={answer.id}
-						id={answer.id}
-					/>
-				))}
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className='answer-list'>
+                {this.props.answers.map(answer => (
+                    <Answer
+                        key={answer.id}
+                        id={answer.id}
+                    />
+                ))}
+            </div>
+        );
+    }
 }
