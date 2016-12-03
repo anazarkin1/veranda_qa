03ee706ebff78d17bb3dbcb84a5e7d920faff098
azenzion <alaric.mckenzie.boone@gmail.com>
2016-12-02 20:25:45 -0500

    alex's update of AnswerNew and AnswersThread

--- app/src/js/components/AnswerNew.js
+++ app/src/js/components/AnswerNew.js
@@ -1,65 +1,63 @@
 import React, {Component} from "react";
 import RichEditor from "./RichEditor";
 import AnswerNewControls from "./AnswerNewControls";
+import axios from "axios";
 
 
 export default class AnswerNew extends Component {
-    constructor() {
-        super();
+   constructor() {
+       super();
 
-        this.state = {
-            value: "",
-            is_anon: false
-        };
-        this.onValueChanged = this.onValueChanged.bind(this);
-        this.onCancel = this.onCancel.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onIsAnonChanged = this.onIsAnonChanged.bind(this);
-    }
+       this.state = {
+           value: "",
+           is_anon: false
+       };
+       this.onValueChanged = this.onValueChanged.bind(this);
+       this.onCancel = this.onCancel.bind(this);
+       this.onSubmit = this.onSubmit.bind(this);
+       this.onIsAnonChanged = this.onIsAnonChanged.bind(this);
+   }
 
-    onValueChanged(value) {
-        //Copy old state, change its value to new val
-        let newState = this.state.slice();
-        newState.value = value;
-        this.setState({newState});
-        console.log(value);
-    }
+   onValueChanged(value) {
+       this.setState({value});
+   }
 
-    onIsAnonChanged() {
-        this.setState({is_anon: !this.state.is_anon});
-        console.log(this.state);
-    }
+   onIsAnonChanged() {
+       this.setState({is_anon: !this.state.is_anon});
+   }
 
-    onCancel() {
+   onCancel() {
 
-    }
+   }
 
-    onSubmit() {
-        //TODO: replace dummy ajax result var 'r'
-        // TODO:send ajax post and check return value
-        let r = {
-            id: 99,
-            created_at: ((+new Date()) / 1000) - 1000,
-            content: 'New Answer 1',
-            created_by: 'user999'
-        };
-        if (true) {
-            this.props.onPostSuccess(r);
-        }
-    }
+   onSubmit() {
+       //TODO: replace dummy ajax result var 'r'
+       // TODO:send ajax post and check return value
 
-    render() {
-        return (
-            <div className='answer-new'>
-                <RichEditor
-                    changeHandler={this.onValueChanged}
-                />
-                <AnswerNewControls
-                    onSubmit={this.onSubmit}
-                    onCancel={this.onCancel}
-                    onIsAnonChanged={this.onIsAnonChanged}
-                />
-            </div>
-        );
-    }
+
+       let r = {
+           id: 99,
+           created_at: ((+new Date()) / 1000) - 1000,
+           content: 'New Answer 1',
+           created_by: 'user999'
+       };
+       if (true) {
+           this.props.onPostSuccess(r);
+       }
+   }
+
+   render() {
+       return (
+           <div className='answer-new'>
+               <RichEditor
+                   onValueChanged={this.onValueChanged}
+               />
+               <AnswerNewControls
+                   onSubmit={this.onSubmit}
+                   onCancel={this.onCancel}
+                   onIsAnonChanged={this.onIsAnonChanged}
+               />
+           </div>
+       );
+   }
 }
--- app/src/js/components/AnswersThread.js
+++ app/src/js/components/AnswersThread.js
@@ -3,47 +3,47 @@ import AnswerNew from "./AnswerNew";
 import AnswerList from "./AnswerList";
 
 /*
- Wrapper around answerNew and answerlist in a thread
- */
+Wrapper around answerNew and answerlist in a thread
+*/
 export default class AnswersThread extends Component {
-    constructor() {
-        super();
-        this.state = {
-            answers: [
-                {id: 1, created_at: ((+new Date()) / 1000) - 1000, content: 'ANswer 1', created_by: 'user1'},
-                {id: 2, created_at: ((+new Date()) / 1000) - 5000, content: 'Answer 2', created_by: 'user1'},
-                {id: 3, created_at: ((+new Date()) / 1000) - 10000, content: 'Answer 3', created_by: 'user2'}
-            ]
-        };
-        this.onPostSuccess = this.onPostSuccess.bind(this);
-    }
+   constructor() {
+       super();
+       this.state = {
+           answers: [
+               {id: 1, created_at: ((+new Date()) / 1000) - 1000, content: 'Content of ANswer 1', created_by: 'user1'},
+               {id: 2, created_at: ((+new Date()) / 1000) - 5000, content: 'Content of Answer 2', created_by: 'user1'},
+               {id: 3, created_at: ((+new Date()) / 1000) - 10000, content: 'COntent of Answer 3', created_by: 'user2'}
+           ]
+       };
+       this.onPostSuccess = this.onPostSuccess.bind(this);
+   }
 
-    componentDidMount() {
-        //TODO:Fetch all answers for this thread and this.setState with them
-    }
+   componentDidMount() {
+       //TODO:Fetch all answers for this thread and this.setState with them
+   }
 
-    onPostSuccess(newPost) {
-        //add newPost to the front ofanswersList
-        if (Object.keys(newPost).length > 0) {
-            let newAnswers = this.state.answers.slice()
-            newAnswers.unshift(newPost);
-            this.setState({answers: newAnswers});
-        } else {
-            //TODO: log error?
-        }
-    }
+   onPostSuccess(newPost) {
+       //add newPost to the front ofanswersList
+       if (Object.keys(newPost).length > 0) {
+           let newAnswers = this.state.answers.slice()
+           newAnswers.unshift(newPost);
+           this.setState({answers: newAnswers});
+       } else {
+           //TODO: log error?
+       }
+   }
 
 
-    render() {
-        return (
-            <div className='answers'>
-                <AnswerNew
-                    onPostSuccess={this.onPostSuccess}
-                />
-                <AnswerList
-                    answers={this.state.answers}
-                />
-            </div>
-        );
-    }
+   render() {
+       return (
+           <div className='answers'>
+               <AnswerNew
+                   onPostSuccess={this.onPostSuccess}
+               />
+               <AnswerList
+                   answers={this.state.answers}
+               />
+           </div>
+       );
+   }
 }
